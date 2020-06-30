c061395dbd8cd1f6ab5bc24defd5c2d5b2138136
431305450 <431305450@qq.com>
2020-06-30 19:09:22 +0800

    将用户名写道mysql写入cookie

--- src/main/java/life/dingdang/community/community/controller/AuthorizeController.java
+++ src/main/java/life/dingdang/community/community/controller/AuthorizeController.java
@@ -11,9 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.UUID;
+
 
 @Controller
 public class AuthorizeController {
@@ -34,33 +37,35 @@ public class AuthorizeController {
     private UserMapper userMapper;
 
     @GetMapping("/callback")
-    //第一步调用Authorize接口接受传过来的code stateo从页面传过来的
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name="state")String state,
-                           HttpServletRequest request, HttpServletResponse response//可以用这个拿到session
-                           ){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request,
+                           HttpServletResponse response){//把上下文中的request放在request供我们使用
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+//        githubProvider.getAccessToken(accessTokenDTO);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri(redirectUrl);
         accessTokenDTO.setClient_id(clientId);
-        accessTokenDTO.setState(state);
         accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUrl);
+        accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser githubUser = githubProvider.getUser(accessToken);
-        if(githubUser !=null){
+        GithubUser guser = githubProvider.getUser(accessToken);
+        if(guser!=null){//如果登录不为空，写Cookie和session,否则登陆失败
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
-            user.setName(githubUser.getName());
-            user.setAccountId(String.valueOf((githubUser.getId())));
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(guser.getName());
+            user.setAccountId(String.valueOf(guser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
-            userMapper.insert(user);
-            request.getSession().setAttribute("user",githubUser);
-            return "redirect:/";
-            //登录成功,写cookie和session
-        }else{
+            userMapper.insert(user);//把user写入数据库便相当于写入session
+            //登陆成功，写入cookie和session
+            response.addCookie(new Cookie("token",token));
+            request.getSession().setAttribute("guser",guser);//把user对象放到session里面
             return "redirect:index";
-            //登录失败，重新登录
+        }else{
+            return "redirect:index"; //重新登录
         }
+
     }
 }
--- src/main/java/life/dingdang/community/community/mapper/UserMapper.java
+++ src/main/java/life/dingdang/community/community/mapper/UserMapper.java
@@ -3,9 +3,15 @@ package life.dingdang.community.community.mapper;
 import life.dingdang.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified)values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+    @Select("select *from user where token= #{token}")
+    User findByToken(@Param("token") String token);
 }
